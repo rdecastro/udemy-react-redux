5caa7abf16428979a3f7e09b632f6f073f2949a9
Ray Joseph de Castro <zengineer@me.com>
2016-04-13 12:21:31 -0700

    feature. implemented search term to search on keypress.

--- src/index.js
+++ src/index.js
@@ -11,26 +11,31 @@ const API_KEY = 'AIzaSyB5dcpDUD_7P1fSmcjYRCnRXGgG4CP6-Cw';
 class App extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             videos: [],
             selectedVideo: null
         };
-        
-        YTSearch({key: API_KEY, term: 'surfboards'}, videos => { 
+
+        this.videoSearch('surfing');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
         });
     }
+
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange = {term => {this.videoSearch(term)}}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
-                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})} 
+                    onVideoSelect = {selectedVideo => this.setState({ selectedVideo }) }
                     videos={this.state.videos} />
             </div>
         );
